32511c1241a49423e9a8560c92e9f709942b3f97
Vitor Valente <ovitorvalente@gmail.com>
2025-05-15 23:29:43 -0300

    feat: adiciona o component hero.tsx que possui a sessao principal do site

---
+++ src/app/components/HeroSection/hero.tsx
@@ -0,0 +1,24 @@
+import { Logo } from "@/components/internal/logo";
+import { ShortenForm } from "./shorten-form";
+
+export function HeroSection() {
+  return (
+    <>
+      <section className="flex items-center justify-center mx-auto">
+        <div className="flex w-md flex-col gap-4">
+          <div className="flex items-center justify-center gap-4">
+            <h1 className="text-4xl font-bold">Bem-vindo ao</h1>
+            <Logo />
+          </div>
+          <span className="text-center opacity-80">
+            Insira o link que deseja encurtar abaixo e gere uma versão compacta
+            em segundos.
+          </span>
+
+          <ShortenForm />
+          {/* <AlertInfo /> */}
+        </div>
+      </section>
+    </>
+  );
+}
