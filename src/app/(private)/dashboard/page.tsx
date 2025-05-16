401e19d38499ebb8edba502515d1891cde7e3704
Vitor Valente <ovitorvalente@gmail.com>
2025-05-15 23:32:31 -0300

    feat: adiciona a pagina dashboard 'em construcao no momento'

---
+++ src/app/(private)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+export default function Dashboard() {
+  return (
+    <>
+      <main className="flex items-center justify-center h-screen">
+        <h1>Dashboard</h1>
+      </main>
+      main
+    </>
+  );
+}
