0cdeeb69354c72e77a7885833097e21b3c5934e7
Vitor Valente <ovitorvalente@gmail.com>
2025-05-15 23:28:50 -0300

    feat: ajustes na estrutura do component logo.tsx

--- src/components/internal/logo.tsx
+++ src/components/internal/logo.tsx
@@ -1,18 +1,21 @@
 import Image from "next/image";
 import icon from "../../../public/icon.png";
+import Link from "next/link";
 
 export function Logo() {
   return (
     <>
-      <div className="flex items-center justify-center gap-2">
-        <Image src={icon} alt="Icone de pato" width={32} height={32} />
-        <div className="flex items-center justify-center gap-1 border-b-2 border-amber-300">
-          <span className="bg-amber-300 p-0.5 text-xl font-bold text-background">
-            QCK
-          </span>
-          <span className="text-xl font-bold">.link</span>
+      <Link href={"/"}>
+        <div className="flex items-center justify-center gap-2">
+          <Image src={icon} alt="Icone de pato" width={32} height={32} />
+          <div className="flex items-center justify-center gap-1 border-b-2 rounded-bl-lg border-amber-300">
+            <span className="bg-amber-300 p-0.5 text-xl rounded-md rounded-br-none font-bold text-background">
+              QCK.
+            </span>
+            <span className="text-xl font-black">Link</span>
+          </div>
         </div>
-      </div>
+      </Link>
     </>
   );
 }
