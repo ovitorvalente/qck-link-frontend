e35a71392826f3a7e8d82927bedd9c24c637f220
Vitor Valente <ovitorvalente@gmail.com>
2025-05-15 23:29:57 -0300

    feat: adiciona o component footer.tsx

---
+++ src/app/components/Footer/footer.tsx
@@ -0,0 +1,27 @@
+import { Badge } from "@/components/ui/badge";
+
+export function Footer() {
+  return (
+    <>
+      <footer className="fixed bottom-0 w-full backdrop-blur-3xl border-t border-dashed bg-background/80 z-50">
+        <div className="w-full flex items-center justify-between max-w-[1920px] px-12 py-2 mx-auto">
+          <div className="flex items-center justify-center">
+            <a href="" className="text-sm text-foreground/50">
+              © 2025 - QCK. Link, feito por{" "}
+              <span className="font-bold text-foreground">Vitor Valente</span>
+            </a>
+          </div>
+          <div className="flex items-center justify-center gap-2">
+            <Badge
+              variant={"outline"}
+              className="rounded-full gap-2 hover:bg-green-500/30 transition-all delay-75 duration-300"
+            >
+              <span className="bg-green-500 size-2 rounded-full" />
+              <span>Ativo</span>
+            </Badge>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
