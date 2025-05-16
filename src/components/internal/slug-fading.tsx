3f7ffbe5b0cf4f7a00132acf316f5b14aeee5cf6
Vitor Valente <ovitorvalente@gmail.com>
2025-05-15 23:37:21 -0300

    feat: adiciona o component 'slug-fading.tsx' que realiza um efeito no texto exibido

---
+++ src/components/internal/slug-fading.tsx
@@ -0,0 +1,15 @@
+export function SlugFading() {
+  return (
+    <>
+      <span className="text-md font-normal max-xl:hidden">
+        <span className="fade-word">O</span>{" "}
+        <span className="fade-word">encurtador</span>{" "}
+        <span className="fade-word">de</span>{" "}
+        <span className="fade-word">URLs</span>{" "}
+        <span className="fade-word">ágil</span>{" "}
+        <span className="fade-word">e</span>{" "}
+        <span className="fade-word">inteligente</span>
+      </span>
+    </>
+  );
+}
