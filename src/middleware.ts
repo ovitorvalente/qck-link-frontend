7022fd444d80fc75ac10b6aa571784facc42ad10
Vitor Valente <ovitorvalente@gmail.com>
2025-05-15 23:27:25 -0300

    feat: adiciona o middleware.ts para protecao das rotas

---
+++ src/middleware.ts
@@ -0,0 +1,72 @@
+import {
+  type MiddlewareConfig,
+  type NextRequest,
+  NextResponse,
+} from "next/server";
+
+const publicRoutes = [
+  {
+    path: "/",
+    whenAuthenticated: "next",
+  },
+  {
+    path: "/auth",
+    whenAuthenticated: "redirect",
+  },
+  {
+    path: "/changelog",
+    whenAuthenticated: "next",
+  },
+  {
+    path: "/about",
+    whenAuthenticated: "next",
+  },
+  {
+    path: "/faq",
+    whenAuthenticated: "next",
+  },
+];
+
+const REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE = "/auth";
+const REDIRECT_WHEN_AUTHENTICATED_ROUTE = "/dashboard";
+
+export function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+  const publicRoute = publicRoutes.find((route) => route.path === path);
+  const authToken = request.cookies.get("token");
+
+  if (!authToken && publicRoute) {
+    return NextResponse.next();
+  }
+
+  if (!authToken && !publicRoute) {
+    const redirectUrl = request.nextUrl.clone();
+    redirectUrl.pathname = REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE;
+    return NextResponse.redirect(redirectUrl);
+  }
+
+  if (
+    authToken &&
+    publicRoute &&
+    publicRoute.whenAuthenticated === "redirect"
+  ) {
+    const redirectUrl = request.nextUrl.clone();
+    redirectUrl.pathname = REDIRECT_WHEN_AUTHENTICATED_ROUTE;
+    return NextResponse.redirect(redirectUrl);
+  }
+
+  return NextResponse.next();
+}
+
+export const config: MiddlewareConfig = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
+};
