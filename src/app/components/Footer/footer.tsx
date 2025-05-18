0899c19f73f957b74c0f5ffe273dd44fbeba02c4
Vitor Valente <ovitorvalente@gmail.com>
2025-05-18 00:14:44 -0300

    feat: ajusta o link do GitHub no rodapé e melhora a responsividade

--- src/app/components/Footer/footer.tsx
+++ src/app/components/Footer/footer.tsx
@@ -4,9 +4,15 @@ export function Footer() {
   return (
     <>
       <footer className="fixed bottom-0 w-full backdrop-blur-3xl border-t border-dashed bg-background/80 z-50">
-        <div className="w-full flex items-center justify-between max-w-[1920px] px-12 py-2 mx-auto">
+        <div className="w-full flex items-center justify-between max-w-[1920px] px-12 max-md:px-4 py-2 mx-auto">
           <div className="flex items-center justify-center">
-            <a href="" className="text-sm text-foreground/50">
+            <a
+              href="https://github.com/ovitorvalente"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Abrir link do github"
+              className="text-sm text-foreground/50"
+            >
               © 2025 - QCK. Link, feito por{" "}
               <span className="font-bold text-foreground">Vitor Valente</span>
             </a>
