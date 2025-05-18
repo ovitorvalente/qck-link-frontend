17bb3afbdbaa6e45a0def4de6bc315c2f1c5e079
Vitor Valente <ovitorvalente@gmail.com>
2025-05-18 00:14:18 -0300

    feat: reformula a página de changelog com novos componentes e layout

--- src/app/(public)/changelog/page.tsx
+++ src/app/(public)/changelog/page.tsx
@@ -1,7 +1,30 @@
-export default function Changelog() {
+import { ChangelogOptions } from "./components/changelog-options";
+import ChangelogView from "./components/changelog-view";
+import { ChangelogProvider } from "./contexts /ChangelogContext";
+
+export default function ChangelogPage() {
   return (
-    <main className="flex items-center justify-center h-screen">
-      <h1>Changelog</h1>
-    </main>
+    <>
+      <div className="min-h-screen max-w-4xl mx-auto max-md:p-6 my-32">
+        <header className="flex flex-col gap-4">
+          <h1 className="text-4xl font-bold text-foreground">
+            Notas de Lançamento
+          </h1>
+          <p className="text-foreground/80">
+            Fique por dentro das últimas atualizações do QCK.Link! Desde a{" "}
+            <a href="#1.0.0-beta" className="font-bold hover:underline">
+              versão 1.0.0 (Beta)
+            </a>
+            , estamos trabalhando para tornar o encurtador de links cada vez
+            mais eficiente, seguro e funcional. Agradecemos todo o apoio e
+            feedback durante essa fase inicial de testes! 🚀❤️
+          </p>
+        </header>
+        <ChangelogProvider>
+          <ChangelogOptions />
+          <ChangelogView />
+        </ChangelogProvider>
+      </div>
+    </>
   );
 }
