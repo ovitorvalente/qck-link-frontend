9670b26d1e7b904e763fca8d05586a5f8fd3a1bd
Vitor Valente <ovitorvalente@gmail.com>
2025-05-19 19:37:17 -0300

    feat: adiciona componente RegisterVisit na página inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { HeroSection } from "./components/HeroSection/hero";
+import { RegisterVisit } from "./components/ui/register-visit";
 
 export default function Home() {
   return (
     <main className="flex w-full max-w-[1920px] mx-auto h-screen">
       <HeroSection />
+      <RegisterVisit page={"/"} />
     </main>
   );
 }
