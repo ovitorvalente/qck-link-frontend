190e9ceb43d4ab7e8ae3324bf1566c60535eebaa
Vitor Valente <ovitorvalente@gmail.com>
2025-05-19 21:11:04 -0300

    fix: remove importação de imagem e ajusta estilo do logo no componente Logo

--- src/components/internal/logo.tsx
+++ src/components/internal/logo.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-import icon from "../../../public/icon.png";
 import Link from "next/link";
 
 export function Logo() {
@@ -7,9 +5,8 @@ export function Logo() {
     <>
       <Link href={"/"}>
         <div className="flex items-center justify-center gap-2">
-          <Image src={icon} alt="Icone de pato" width={32} height={32} />
-          <div className="flex items-center justify-center gap-1 border-b-2 rounded-bl-lg border-amber-300">
-            <span className="bg-amber-300 p-0.5 text-xl rounded-md rounded-br-none font-bold text-background">
+          <div className="flex items-center justify-center gap-1 border-b-2 rounded-bl-md border-amber-300">
+            <span className="bg-amber-300 p-0.5 text-xl rounded-sm rounded-br-none font-bold text-background">
               QCK.
             </span>
             <span className="text-xl font-black">Link</span>
