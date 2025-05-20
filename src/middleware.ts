227344e9fdb90260e64001b3f64b0860952aa59d
Vitor Valente <ovitorvalente@gmail.com>
2025-05-19 23:43:16 -0300

    refactor: simplifica a estrutura do middleware e melhora a legibilidade do código

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,33 +5,27 @@ import {
 } from "next/server";
 
 const publicRoutes = [
-  {
-    path: "/",
-    whenAuthenticated: "next",
-  },
-  {
-    path: "/auth",
-    whenAuthenticated: "redirect",
-  },
-  {
-    path: "/changelog",
-    whenAuthenticated: "next",
-  },
-  {
-    path: "/about",
-    whenAuthenticated: "next",
-  },
-  {
-    path: "/faq",
-    whenAuthenticated: "next",
-  },
+  { path: "/", whenAuthenticated: "next" },
+  { path: "/auth", whenAuthenticated: "redirect" },
+  { path: "/changelog", whenAuthenticated: "next" },
+  { path: "/about", whenAuthenticated: "next" },
+  { path: "/faq", whenAuthenticated: "next" },
 ];
 
 const REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE = "/auth";
 const REDIRECT_WHEN_AUTHENTICATED_ROUTE = "/dashboard";
 
+const isShortCode = (path: string) => /^\/[a-zA-Z0-9]{5,20}$/.test(path);
+
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
+
+  if (isShortCode(path)) {
+    return NextResponse.redirect(
+      `https://qck-link-backend.onrender.com${path}`
+    );
+  }
+
   const publicRoute = publicRoutes.find((route) => route.path === path);
   const authToken = request.cookies.get("token");
 
@@ -60,13 +54,6 @@ export function middleware(request: NextRequest) {
 
 export const config: MiddlewareConfig = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
-     */
     "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
   ],
 };
